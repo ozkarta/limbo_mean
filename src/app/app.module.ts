d0a579335ff2c4e74fd8255b2fcd6b7e70a885de
ozkarta <ozbegi1@gmail.com>
2018-01-28 06:23:25 +0400

    navbar implementation is improved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app.routes';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {BusyModule} from 'angular2-busy';
 
 import {AppComponent} from './app.component';
 import {VisitorHomeComponent} from './visitor/home/home.component'
 import {VisitorNavComponent} from './visitor/nav/nav.component';
-import {AppRoutingModule} from './app.routes';
+import {MainNavComponent} from './shared/nav/nav.component'
 
 import {AppService} from './shared/service/app.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainNavComponent,
+
     VisitorHomeComponent,
     VisitorNavComponent
   ],
---
+++ src/app/shared/nav/nav.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+@Component({
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.style.css']
+})
+export class MainNavComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+}
