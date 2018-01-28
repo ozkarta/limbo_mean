98c0bdca43a4048900ccf270c6387cbf65932395
ozkarta <ozbegi1@gmail.com>
2018-01-28 06:39:17 +0400

    Setting dynamic tytles

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,15 +1,17 @@
 import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+
 @Component({
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.style.css']
 })
 export class MainNavComponent implements OnInit {
 
-  constructor() {
+  constructor(private titleService: Title) {
 
   }
 
   ngOnInit() {
-
+    this.titleService.setTitle('Limbo MEAN app');
   }
 }
