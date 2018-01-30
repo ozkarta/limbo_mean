ae746036acc38e9c56751b7575e80f61903e5349
ozkarta <ozbegi1@gmail.com>
2018-01-31 02:29:14 +0400

    registration works

--- api/v1/components/user/user.controller.js
+++ api/v1/components/user/user.controller.js
@@ -1,6 +1,6 @@
 module.exports = function (express) {
 	let router = express.Router();
-	let UserModel = require('./user.model');
+	let UserModel = require('./user.model').model;
 	let jwt = require('jsonwebtoken');
 	let bcrypt = require('bcryptjs');
 	let config = require('../../shared/config/config');
@@ -11,7 +11,34 @@ module.exports = function (express) {
 
 	router.post('/register', (req, res) => {
 		console.dir(req.body);
-		return res.status(200).json({});
+		// TODO  validate response
+		// return status if validation fails
+
+		UserModel.findOne({ email: req.body.email }, (err, result) => {
+			if (err) {
+				// TODO return status message
+				return res.status(500).json({});
+			}
+			if (result) {
+				// TODO  return message
+				// email is already used
+				return res.status(400).json({});
+			}
+
+			let user = new UserModel(req.body);
+			user.save((err, savedUser) => {
+				if (err) {
+					// TODO return status message
+					return res.status(500).json({});
+				}
+
+				let token = jwt.sign({ id: user._id }, config.secret, {
+					expiresIn: 86400 // expires in 24 hours
+				});
+				res.status(200).send({ auth: true, token: token });
+			});
+		});
+
 	});
 
 	router.put('/', (req, res) => {
