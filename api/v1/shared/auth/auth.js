c97b99591e81031c0116702e22dd458970b696a9
ozkarta <ozbegi1@gmail.com>
2018-02-02 01:53:10 +0400

    auth.js file is added, mockup is done, no implementation

---
+++ api/v1/shared/auth/auth.js
@@ -0,0 +1,19 @@
+module.exports.isEmployer = (req, res, next) => {
+
+};
+
+module.exports.isEmployee = (req, res, next) => {
+
+};
+
+module.exports.isIndividual = (req, res, next) => {
+
+};
+
+module.exports.isCompany = (req, res, next) => {
+
+};
+
+module.exports.isSigned = (req, res, next) => {
+
+};
