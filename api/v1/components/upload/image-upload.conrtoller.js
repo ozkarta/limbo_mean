2f6a8ce48566ce01e53b268c20e324100d2519ad
ozkarta <ozbegi1@gmail.com>
2018-02-09 22:40:41 +0400

    image upload works

--- api/v1/components/upload/image-upload.conrtoller.js
+++ api/v1/components/upload/image-upload.conrtoller.js
@@ -1,13 +1,17 @@
 module.exports = function (express) {
 	let router = express.Router();
+	let multer  = require('multer');
+	let sharedFolderName = 'data'
+	imageUploadPath = '/uploads/images'
+	let upload = multer({ dest: `${sharedFolderName}${imageUploadPath}` });
 
-	router.post('/', (req, res) => {
+	router.post('/', upload.single('file'), (req, res) => {
 
-		console.log('payload...')
-		console.dir(req.payload);
-		console.log('Body...');
-		console.dir(req.body);
-		return res.status(200).json({});
+		let file = {};
+		if (req.file) {
+			file.publicPath = `${req.file.destination.replace(sharedFolderName, '')}/${req.file.filename}`;
+		}
+		return res.status(200).json(file);
 	});
 
 	return router;
--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'data')));
 app.use('/doc', express.static(path.join(__dirname, 'doc')));
 
 
